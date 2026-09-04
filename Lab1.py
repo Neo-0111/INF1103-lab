86ed73bdc15b109438aa8f28deec64b4260035a7
Neo Jun Hui <2605731@sit.singaporetech.edu.sg>
2026-09-04 14:16:57 +0800

    Profile update

--- Lab1.py
+++ Lab1.py
@@ -2,3 +2,11 @@ print ("================")
 print ("Welcome here")
 print ("My first post!")
 print ("================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 1
+
+print (username, "username")
+print (bio, "bio")
+print (followers, "followers")
