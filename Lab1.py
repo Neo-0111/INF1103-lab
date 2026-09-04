87dacc887b2ddc91944845e9c1c565faf8c6ad28
Neo Jun Hui <2605731@sit.singaporetech.edu.sg>
2026-09-04 14:19:53 +0800

    Followerupdate

--- Lab1.py
+++ Lab1.py
@@ -10,3 +10,12 @@ followers = 1
 print (username, "username")
 print (bio, "bio")
 print (followers, "followers")
+
+followers += 50
+print ("Day 1 followers", followers)
+
+followers += 100
+print ("Day 2 followers", followers)
+
+followers += 189
+print ("Day 3 followers", followers)
